74e1c154637afd755b37fae0b77f24a8b6c792a1
shonshon31 <salvador.258572@ortigas-cainta.sti.edu.ph>
2024-06-09 23:40:55 +0800

    nega

--- app/src/main/java/com/example/loginandregistration/MainMenu.java
+++ app/src/main/java/com/example/loginandregistration/MainMenu.java
@@ -11,21 +11,21 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainMenu extends AppCompatActivity {
 
+    private RadioGroup szAme, szCapu, szMacchi;
     FirebaseAuth auth;
     RadioButton tallAme, grandeAme, ventiAme, tallCapu, grandeCapu, ventiCapu, tallMacchi, grandeMacchi, ventiMacchi;
     Button button, checkOut, plusAme, minusAme, plusCapu, minusCapu, plusMacchi, minusMacchi;
-    TextView textView;
+    TextView textView, quanAme, quanCapu, quanMacchi;
+
     String selectedCoffee;
     FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,15 +36,9 @@ public class MainMenu extends AppCompatActivity {
         button = findViewById(R.id.logout);
         textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
-        tallAme = (RadioButton)findViewById(R.id.tallAme);
-        grandeAme = (RadioButton)findViewById(R.id.grandeAme);
-        ventiAme = (RadioButton)findViewById(R.id.ventiAme);
-        tallCapu = (RadioButton)findViewById(R.id.tallAme);
-        grandeCapu = (RadioButton)findViewById(R.id.grandeAme);
-        ventiCapu = (RadioButton)findViewById(R.id.ventiAme);
-        tallMacchi = (RadioButton)findViewById(R.id.tallAme);
-        grandeMacchi = (RadioButton)findViewById(R.id.grandeAme);
-        ventiMacchi = (RadioButton)findViewById(R.id.ventiAme);
+        szAme = findViewById(R.id.szAme);
+        szCapu = findViewById(R.id.szCapu);
+        szMacchi = findViewById(R.id.szMacchi);
         checkOut = findViewById(R.id.btn_checkout);
         plusAme = findViewById(R.id.plusame);
         minusAme = findViewById(R.id.minusame);
@@ -52,91 +46,68 @@ public class MainMenu extends AppCompatActivity {
         minusCapu = findViewById(R.id.minuscapu);
         plusMacchi = findViewById(R.id.plusmacchi);
         minusMacchi = findViewById(R.id.minusmacchi);
-      
+        quanAme = findViewById(R.id.quaname);
+        quanCapu = findViewById(R.id.quancapu);
+        quanMacchi = findViewById(R.id.quanmacchi);
 
         checkOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (tallAme.isChecked()) {
-                    selectedCoffee = tallAme.getText().toString();
-                } else if (grandeAme.isChecked()) {
-                    selectedCoffee = grandeAme.getText().toString();
-                } else if (ventiAme.isChecked()) {
-                    selectedCoffee = ventiAme.getText().toString();
-                } else if (tallCapu.isChecked()) {
-                    selectedCoffee = tallCapu.getText().toString();
-                } else if (grandeCapu.isChecked()) {
-                    selectedCoffee = grandeCapu.getText().toString();
-                }
-                  else if (ventiCapu.isChecked()){
-                    selectedCoffee = ventiCapu.getText().toString();
-                }
-                else if (tallMacchi.isChecked()){
-                    selectedCoffee = tallMacchi.getText().toString();
-                }
-                else if (grandeMacchi.isChecked()){
-                    selectedCoffee = grandeMacchi.getText().toString();
-                }
-                else if (ventiMacchi.isChecked()){
-                    selectedCoffee = ventiMacchi.getText().toString();
-                }
-                Toast.makeText(getApplicationContext(), selectedCoffee, Toast.LENGTH_LONG).show();
+                String combinedOrder = getCombinedSelectedOrder();
+                Toast.makeText(MainMenu.this, combinedOrder, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Checkout.class);
+                startActivity(intent);
+                finish();
             }
         });
-
-
 
         plusAme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int quanAme;
-                quanAme++;
+                plusquan_Ame();
             }
         });
 
         minusAme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int quanAme;
-                quanAme--;
+                minusquan_Ame();
             }
         });
+
         plusCapu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                quanCapu++;
+                plusquan_Capu();
             }
         });
 
         minusCapu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                quanCapu--;
+                minusquan_Capu();
             }
         });
 
         plusMacchi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                quanMacchi++;
+                plusquan_Macchi();
             }
         });
 
         minusMacchi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                quanMacchi --;
+                minusquan_Macchi();
             }
         });
 
-
-
-        if (user == null){
+        if (user == null) {
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
             finish();
-        }
-        else{
+        } else {
             textView.setText(user.getEmail());
         }
 
@@ -149,5 +120,72 @@ public class MainMenu extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void plusquan_Ame() {
+        String currentValue = quanAme.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = currentNumber + 1;
+        quanAme.setText(String.valueOf(newNumber));
+    }
+
+    private void minusquan_Ame() {
+        String currentValue = quanAme.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = Math.max(currentNumber - 1, 0);
+        quanAme.setText(String.valueOf(newNumber));
+    }
+
+    private void plusquan_Capu() {
+        String currentValue = quanCapu.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = currentNumber + 1;
+        quanCapu.setText(String.valueOf(newNumber));
+    }
+
+    private void minusquan_Capu() {
+        String currentValue = quanCapu.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = Math.max(currentNumber - 1, 0);
+        quanCapu.setText(String.valueOf(newNumber));
+    }
+
+    private void plusquan_Macchi() {
+        String currentValue = quanMacchi.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = currentNumber + 1;
+        quanMacchi.setText(String.valueOf(newNumber));
+    }
+
+    private void minusquan_Macchi() {
+        String currentValue = quanMacchi.getText().toString();
+        int currentNumber = Integer.parseInt(currentValue);
+        int newNumber = Math.max(currentNumber - 1, 0);
+        quanMacchi.setText(String.valueOf(newNumber));
+    }
+
+    private String getCombinedSelectedOrder() {
+        int selectedId1 = szAme.getCheckedRadioButtonId();
+        int selectedId2 = szCapu.getCheckedRadioButtonId();
+        int selectedId3 = szMacchi.getCheckedRadioButtonId();
+
+        String selectedValue1 = "";
+        String selectedValue2 = "";
+        String selectedValue3 = "";
+
+        if (selectedId1 != -1) {
+            RadioButton selectedRadioButton1 = findViewById(selectedId1);
+            selectedValue1 = selectedRadioButton1.getText().toString();
+        }
+        if (selectedId2 != -1) {
+            RadioButton selectedRadioButton2 = findViewById(selectedId2);
+            selectedValue2 = selectedRadioButton2.getText().toString();
+        }
+        if (selectedId3 != -1) {
+            RadioButton selectedRadioButton3 = findViewById(selectedId3);
+            selectedValue3 = selectedRadioButton3.getText().toString();
+        }
+
+        return "Americano: " + selectedValue1 + ", Cappuccino: " + selectedValue2 + ", Macchiato: " + selectedValue3;
     }
 }
